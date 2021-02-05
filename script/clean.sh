aa8f817ba21e3830d00f3f3390f1e365dadbea24
Jia <49358172+craftslab@users.noreply.github.com>
2021-02-05 12:50:38 +0800

    Update clean.sh

--- script/clean.sh
+++ script/clean.sh
@@ -4,8 +4,8 @@ chmod 644 .gitignore .pre-commit-config.yml .travis.yml
 chmod 644 LICENSE Makefile MANIFEST.in README.md requirements.txt setup.cfg tox.ini
 chmod 644 metrics.py setup.py
 
-find metalmetrics tests -name "*.json" -exec chmod 644 {} \;
 find metalmetrics tests -name "*.py" -exec chmod 644 {} \;
+find metalmetrics tests -name "*.yml" -exec chmod 644 {} \;
 find . -name "*.pyc" ! -path "*.venv*" -exec rm -rf {} \;
 find . -name "*.sh" ! -path "*.venv*" -exec chmod 755 {} \;
 find . -name "__pycache__" ! -path "*.venv*" -exec rm -rf {} \;
